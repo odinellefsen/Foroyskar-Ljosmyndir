58065c4dd6bc71320f2b0bf4c84691dd6d803bfc
odinellefsen <49004766+odinellefsen@users.noreply.github.com>
2021-09-14 10:47:59 +0100

    for loop push into array yey

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
-for(let i = 0; i <= 10; i++) {
-    console.log(i);
+counter = (num) => {
+    counting = [];
+
+    for(let i = 10; i >= num; i--) {
+        counting.push(i);
+    }
+    return counting;
 }
