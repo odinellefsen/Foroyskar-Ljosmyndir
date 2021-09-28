da75de547d0719d26bab455ab7113dbef3ce7e7d
odinellefsen <49004766+odinellefsen@users.noreply.github.com>
2021-09-28 14:16:51 +0100

    hamburger

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => { 
+  navbar.classList.toggle('change');
+})
+
 // countFromZero funktiónin telur frá 0 til talið man koyrir í funktiónina
 countFromZero = (num) => {
     counting = [];
